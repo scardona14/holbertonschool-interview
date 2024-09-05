8d8ea64d47e8dc01fd20d14b3e65042c53fd0e94
scardona14 <cardanasean123@gmail.com>
2024-09-05 14:11:37 -0400

    Arrange the code so It will be the correct output

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -13,18 +13,23 @@ def canUnlockAll(boxes):
     """
 
     canUnlockAll = False
-    keys = [0]
-    visited = set()
+    keys = {0: True}
+    n_boxes = len(boxes)
+    while(True):
 
-    while keys:
-        box = keys.pop(0)
-        visited.add(box)
+        n_keys = len(keys)
 
-        for key in boxes[box]:
-            if key not in visited and key < len(boxes):
-                keys.append(key)
+        for i in range(len(boxes)):
+            if boxes[i] and keys.get(i, False):
+                for j in boxes[i]:
+                    if j < n_boxes:
+                        keys[j] = True
+                    boxes[i] = None
 
-    if len(visited) == len(boxes):
+        if not(len(keys) > n_keys):
+            break
+
+    if n_keys == len(boxes):
         canUnlockAll = True
 
     return canUnlockAll
