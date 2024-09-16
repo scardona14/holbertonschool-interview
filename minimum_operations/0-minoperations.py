3f31dd8df4b6e6e830752720de16518a76340734
scardona14 <cardanasean123@gmail.com>
2024-09-16 12:52:20 -0400

    Corrected the output

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -2,25 +2,26 @@
 
 """ Minimum Operations """
 
+
 def minOperations(n):
     """
-    Calculates the minimum number of operations needed to get exactly n 'H' characters in the file.
+    In a text file, there is a single character H. Your text editor can execute
+    only two operations in this file: Copy All and Paste. Given a number n,
+    write a method that calculates the fewest number of operations needed to
+    result in exactly n H characters in the file.
 
-    Parameters:
-    n (int): The desired number of 'H' characters.
-
-    Returns:
-    int: The minimum number of operations required to achieve exactly n 'H' characters.
-         Returns 0 if n is impossible to achieve.
+    Returns an integer
+    If n is impossible to achieve, returns 0
     """
     if not isinstance(n, int):
         return 0
-    
+
     op = 0
     i = 2
     while (i <= n):
         if not (n % i):
+            n = int(n / i)
             op += i
-            i = i
+            i = 1
         i += 1
     return op
