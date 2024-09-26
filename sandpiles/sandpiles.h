8a2b47d27740b8889c8029cf3132cd0e84c830fe
scardona14 <cardanasean123@gmail.com>
2024-09-26 11:25:27 -0400

    Sandpiles

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -4,5 +4,9 @@
 #include <stdio.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void _print_grid(int grid[3][3]);
+void grid_sum(int grid1[3][3], int grid2[3][3]);
+int is_stable(int grid[3][3]);
+void toppling(int grid[3][3]);
 
 #endif /* SANDPILES_H */
