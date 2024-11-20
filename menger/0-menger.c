e34decbba4c7f87b35d873c290c877d0cf88389c
scardona14 <cardanasean123@gmail.com>
2024-11-20 11:04:52 -0400

    Menger

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -2,52 +2,48 @@
 #include <stdio.h>
 #include <math.h>
 
+
 /**
- * one - Checks if indices are (1, 1), if so, then square is blank
+ * One - Checks if indices are (1, 1), if so, then square is blank
  *
  * @i: Row Index
  * @j: Column Index
  *
  * Return: 1 if (1, 1), 0 otherwise
  */
-int one(int i, int j)
+
+int One(int i, int j)
 {
-    while (i != 0 && j != 0)
-    {
-        if (i % 3 == 1 && j % 3 == 1)
-            return (0);
+	while (i != 0 && j != 0)
+	{
+		if (i % 3 == 1 && j % 3 == 1)
+			return (0);
 
-        i /= 3;
-        j /= 3;
-    }
-
-    return (1);
+		i /= 3, j /= 3;
+	}
+	return (1);
 }
 
 /**
  * menger - Entry point
  *
- * @level: Level of the Menger Sponge to draw
+ * @level: level of the Menger Sponge to draw
  *
  * Return: Nothing
  */
 void menger(int level)
 {
-    int i, j, limit;
+	int i, j, limit;
 
-    if (level < 0)
-        return;
+	if (level < 0)
+		return;
 
-    limit = pow(3, level);
-    for (i = 0; i < limit; i++)
-    {
-        for (j = 0; j < limit; j++)
-        {
-            if (one(i, j) == 1)
-                printf("%c", '#');
-            else
-                printf("%c", ' ');
-        }
-        printf("\n");
-    }
+	for (i = 0, limit = pow(3, level); i < limit; i++)
+	{
+		for (j = 0; j < limit; j++)
+		{
+			One(i, j) == 1 ? printf("%c", '#') : printf("%c", ' ');
+		}
+		printf("\n");
+	}
 }
