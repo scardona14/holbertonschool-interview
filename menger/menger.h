6b491be8035961f36706fef653d431d21e81cad3
scardona14 <cardanasean123@gmail.com>
2024-11-20 10:56:39 -0400

    Menger sponge

--- menger/menger.h
+++ menger/menger.h
@@ -1,6 +1,13 @@
 #ifndef MENGER_H
 #define MENGER_H
 
+/**
+ * menger - Draws a 2D Menger Sponge.
+ *
+ * @level: The level of the Menger Sponge to draw.
+ *
+ * Return: Nothing.
+ */
 void menger(int level);
 
 #endif /* MENGER_H */
